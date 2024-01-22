3afc5628615b2114d2af1ae1f1ab2ca3142b966d
billow <billow.fun@gmail.com>
2024-01-22 20:16:52 +0800

    Fix Nop

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,27 @@ use std::fs;
 use std::io::{BufRead, BufReader, Error};
 use regex::{Captures, Regex};
 use lazy_static::lazy_static;
-use pl_lens::{lens, Lens, Lenses};
 
 lazy_static! {
-    static ref R: Regex = Regex::new(r#"[ ]+([0-9a-f]+):\t(([0-9a-f]+[ ])+)[ \t]+([\w.]+)\t([^\n]+)"#).unwrap();
+    static ref R: Regex = Regex::new(r#"[ ]+([0-9a-f]+):\t(([0-9a-f]+[ ])+)[ \t]+([\w.]+)\t?([^\n]+)?"#).unwrap();
     static ref R_: Regex = Regex::new(r#"[ ]+([0-9a-f]+):\t(([0-9a-f]+[ ])+)"#).unwrap();
     static ref V: Regex = Regex::new(r#"([ \t]*<[^\n]+>[ \t]*)"#).unwrap();
 }
 
-#[derive(Lenses)]
 struct Instruction {
     addr: u64,
     bytes: String,
     inst: String,
-    operands: String,
+    operands: Option<String>,
 }
 
 impl Display for Instruction {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "d \"{} {}\" {} {:#08x}", self.inst, self.operands, self.bytes, self.addr)
+        if let Some(operands) = &self.operands {
+            write!(f, "d \"{} {}\" {} {:#08x}", self.inst, operands, self.bytes, self.addr)
+        } else {
+            write!(f, "d \"{}\" {} {:#08x}", self.inst, self.bytes, self.addr)
+        }
     }
 }
 
@@ -42,23 +44,29 @@ fn main() -> Result<(), Error> {
             let bytes = caps.get(2).unwrap().as_str();
             let bytes_trim = bytes.replace(' ', "");
             let inst = caps.get(4).unwrap().as_str();
-            let operands = caps.get(5).unwrap().as_str();
-            let operands = V.replace_all(operands, |_caps: &Captures| {
-                // println!("{:?}", caps.get(1).unwrap());
-                ""
-            });
+            let operands = caps.get(5).map(
+                |cap| {
+                    V.replace_all(cap.as_str(), |_caps: &Captures| {
+                        // println!("{:?}", caps.get(1).unwrap());
+                        ""
+                    }).to_string()
+                }
+            );
 
             insts.push(Instruction {
                 addr,
                 bytes: bytes_trim.to_string(),
                 inst: inst.to_string(),
-                operands: operands.to_string(),
+                operands,
             });
         } else if let Some(caps) = R_.captures(line.as_str()) {
             let inst = insts.pop().unwrap();
             let bytes = caps.get(2).unwrap().as_str().replace(' ', "");
             let bytes = inst.bytes.clone() + bytes.as_str();
-            insts.push(lens!(Instruction.bytes).set(inst, bytes));
+            insts.push(Instruction {
+                bytes,
+                ..inst
+            });
         }
     }
 
